fc194b25498008e054b55391798b5fdb6591074d
Nathan <pitmannathan@gmail.com>
2015-03-07 20:11:08 +1300

    Second feature. Today highlighting.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # two-time
 Extensions / customisations for the Datacom internal web app called OneTime
+
+
+TODO:
+- Weekday clicking:
+  - Shouldn't be able to trigger a month navigation in calendar
+- Highlight today:
+  - Needs to update in other situations (e.g. when 'Today' is clicked)
+  - Decide on appearance
+- Text search feature
+- Change row height
+- Modalise feature
--- two-time-injection.js
+++ two-time-injection.js
@@ -1,11 +1,6 @@
 (function($) {
-    function enableWeekGridClicking() {
-	var $cal = $('#cal'),
-	    cal = $cal.data('tCalendar'),
-	    $weekGrid = $('#weekgrid'),
-	    weekGrid = $weekGrid.data('tGrid');
-	    
-	$weekGrid.delegate('table > tbody > tr > td:nth-child(1), td:nth-child(2) > label', 'click', function() {
+    function enableWeekGridClicking(calendar, $weekGrid, weekGrid) {
+	$weekGrid.delegate('table:eq(1) > tbody > tr > td:nth-child(1), td:nth-child(2) > label', 'click', function() {
 	    var $tr = $(this).closest('tr'),
 		dayIndex = $tr.index(),
 		boundDateTime = weekGrid.data[dayIndex].weekDateTime;
@@ -13,10 +8,50 @@
 	    cal.value(boundDateTime);
 	});
     }
+
+
+    /**
+     * This function is basically a hack to find the td element (if any) in the calendar which corresponds to today 
+     */
+    function highlightToday($calendar, calendar) {
+	var today = new Date(),
+	    todayDay = today.getDay() + 1;    // Because JS is hilarious
+
+	// We expect 1-2 candidate days, based on day number comparison; one from the current month, and possibly one from the previous/next
+	var $candidateDays = $calendar.find('table tbody td').filter(function() { return $(this).find('a.t-link').text() == todayDay; });
+
+	// Now let's pay attention to month, bearing in mind that the calendar displays up to 3 months at once
+	var $todayTd = $candidateDays.filter(function() { 
+	    var isThisMonth = !$(this).hasClass('t-other-month'),
+		monthOffset = today.getMonth() - calendar.viewedMonth.month();
+
+	    return (monthOffset === 0 && isThisMonth)
+		 || (monthOffset === 1 && todayDay < 15 && !isThisMonth)
+		 || (monthOffset === -1 && todayDay > 15 && !isThisMonth);
+	});
+	
+	$todayTd.addClass('today').addClass('today');
+    }
+
+    
+    function enableTodayHighlighting($calendar, calendar) {
+	var doIt = highlightToday.bind(this, $calendar, calendar);
+	
+	$calendar.bind('navigate change', doIt);
+	$('#DatePicker').change(doIt);
+	doIt();
+    }
 	
 
     $(function() {
-	enableWeekGridClicking();
+	var $cal = $('#cal'),
+	    cal = $cal.data('tCalendar'),
+	    $weekGrid = $('#weekgrid'),
+	    weekGrid = $weekGrid.data('tGrid');
+
+	enableWeekGridClicking(cal, $weekGrid, weekGrid);
+
+	enableTodayHighlighting($cal, cal);
     });
 }(jQuery));
 
