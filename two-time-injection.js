d9de6b46f9950340b4c152b7cc34c2a87622ac06
Nathan <pitmannathan@gmail.com>
2015-03-07 17:56:16 +1300

    First feature implemented. Allow weekday clicking.

--- two-time-injection.js
+++ two-time-injection.js
@@ -1,15 +1,22 @@
-function globalHoratio() {
-    console.log('globalHoratio'); 
-}
-
-
 (function($) {
-    console.log('Welcome to TwoTime!');
+    function enableWeekGridClicking() {
+	var $cal = $('#cal'),
+	    cal = $cal.data('tCalendar'),
+	    $weekGrid = $('#weekgrid'),
+	    weekGrid = $weekGrid.data('tGrid');
+	    
+	$weekGrid.delegate('table > tbody > tr > td:nth-child(1), td:nth-child(2) > label', 'click', function() {
+	    var $tr = $(this).closest('tr'),
+		dayIndex = $tr.index(),
+		boundDateTime = weekGrid.data[dayIndex].weekDateTime;
+
+	    cal.value(boundDateTime);
+	});
+    }
+	
 
     $(function() {
-	console.log($('div').length);
+	enableWeekGridClicking();
     });
 }(jQuery));
-
-
 
